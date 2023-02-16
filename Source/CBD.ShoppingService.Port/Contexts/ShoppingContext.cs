56c8f7027478c796f9c30d23bedd184d00292b04
Dokriseum <dokriseum@pm.me>
2023-02-16 10:25:40 +0100

    DatenbankContext für Datenbank implementiert

---
+++ Source/CBD.ShoppingService.Port/Contexts/ShoppingContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CBD.ShoppingService.Core.Models;
+
+namespace ShoppingService.Port.Contexts;
+
+public class ShoppingContext : DbContext {
+	public ShoppingContext(DbContextOptions options) : base(options) {
+	}
+	public DbSet<Product> Products { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder) {
+		base.OnModelCreating(modelBuilder);
+
+		modelBuilder.Entity<Product>(entity => {
+			entity.HasKey(product => product.Id);
+			entity.HasIndex(product => product.Name).IsUnique();
+		});
+	}
+}
