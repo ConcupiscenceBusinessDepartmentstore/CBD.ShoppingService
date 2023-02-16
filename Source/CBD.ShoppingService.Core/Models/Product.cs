4e76a119dd539da17690bca87778361ed25c3c6b
Dokriseum <dokriseum@pm.me>
2023-02-16 10:09:35 +0100

    Produktklasse implementiert

---
+++ Source/CBD.ShoppingService.Core/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace ShoppingService.Core.Models;
+
+public class Product {
+	public Product() {
+		Id = Guid.NewGuid().ToString();
+	}
+
+	public Product(string name, string description, string price, string quantity) {
+		this.Id = Guid.NewGuid().ToString();
+		this.Name = name;
+		this.Description = description;
+		this.Price = double.Parse(price);
+		this.Quantity = double.Parse(quantity);
+		this.DateTimePosted = DateTime.UtcNow;
+	}
+
+	public string Id { get; set; }
+	public string Name { get; set; }
+	public string Description { get; set; }
+	public DateTime DateTimePosted { get; set; }
+	public double Quantity { get; set; } = 0;
+	public double Price { get; set; } = 0;
+}
