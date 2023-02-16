9eb508ef3cfb48353046bbdb47786771080ba48d
Dokriseum <dokriseum@pm.me>
2023-02-16 10:18:09 +0100

    Serviceschnittstelle für Produkt implementiert

---
+++ Source/CBD.ShoppingService.Core/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using CBD.ShoppingService.Core.Contracts.Requests;
+using CBD.ShoppingService.Core.Contracts.Responses;
+
+namespace CBD.ShoppingService.Core.Services;
+
+public interface IProductService {
+	Task<CreateProductResponse> CreateProductAsync(CreateProductRequest request);
+	Task<DeleteProductResponse> DeleteProductAsync(DeleteProductRequest request);
+	Task<EditProductResponse> EditProductAsync(EditProductRequest request);
+	Task<GetProductResponse> GetProductAsync(GetProductRequest request);
+}
