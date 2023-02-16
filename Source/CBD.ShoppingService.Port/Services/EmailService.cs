d195c087d82581d96a9214f74681a5d24779b071
Dokriseum <dokriseum@pm.me>
2023-02-16 10:57:21 +0100

    Services für Benutzeridentifikation & -interaktion implementiert

---
+++ Source/CBD.ShoppingService.Port/Services/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+using Microsoft.Extensions.Options;
+using CBD.ShoppingService.Core.Options;
+using CBD.ShoppingService.Core.Services;
+
+namespace CBD.ShoppingService.Port.Services; 
+
+public class EmailService : IEmailService {
+	private readonly EmailHostOptions _emailHostOptions;
+	
+	public EmailService(IOptions<EmailHostOptions> emailHostOptions) {
+		this._emailHostOptions = emailHostOptions.Value;
+	}
+
+	public async Task SendEmailAsync(string to, string subject, string message) {
+		var smtpClient = new SmtpClient {
+			Host = this._emailHostOptions.AuthEmailHost,
+			UseDefaultCredentials = false,
+			Credentials = new NetworkCredential(this._emailHostOptions.AuthEmailAddress, this._emailHostOptions.AuthEmailPassword),
+			Port = this._emailHostOptions.AuthEmailHostPort,
+			EnableSsl = true
+		};
+
+		var mailMessage = new MailMessage(this._emailHostOptions.AuthEmailAddress, to) {
+			Subject = subject,
+			Body = message,
+			BodyEncoding = Encoding.UTF8,
+			IsBodyHtml = true
+		};
+
+		await smtpClient.SendMailAsync(mailMessage);
+	}
+}
---
+++ Source/CBD.ShoppingService.Port/Services/IdentityService.cs
@@ -0,0 +1,53 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Options;
+using CBD.ShoppingService.Core.Contracts.Responses.External.Authorization;
+using CBD.ShoppingService.Core.Enums;
+using CBD.ShoppingService.Core.Options;
+using CBD.ShoppingService.Core.Services;
+using CBD.ShoppingService.Port.Extensions;
+using AuthorizeGlobalRoleRequest = CBD.ShoppingService.Core.Contracts.Requests.Authorization.AuthorizeGlobalRoleRequest;
+
+namespace CBD.ShoppingService.Port.Services;
+
+public class IdentityService : IIdentityService {
+	private readonly GatewayOptions optionsGateway;
+	private readonly IdentityServiceOptions optionsIdentityService;
+
+	public IdentityService(IOptions<GatewayOptions> gatewayOptions,
+		IOptions<IdentityServiceOptions> identityServiceOptions) {
+		this.optionsGateway = gatewayOptions.Value;
+		optionsIdentityService = identityServiceOptions.Value;
+	}
+
+	public bool CheckUserHasId(string token, string userId) {
+		var uid = GetUserIdFromToken(token);
+		return (
+			(uid is not null)
+			&& (uid == userId)
+		);
+	}
+
+
+	public async Task<bool> CheckIfUserHasGlobalRole(string token, string roleName)
+	{
+		using var httpClient = new HttpClient();
+		var response = await httpClient.SendPostAsync<AuthorizeGlobalRoleRequest, AuthorizeGlobalRoleResponse>(this.optionsGateway.Routes.AuthorizeRole, new AuthorizeGlobalRoleRequest
+		{
+			Token = token,
+			GlobalRole = roleName,
+		});
+		if (response is null || !response.Succeeded)
+			return false;
+
+		return response.Content!.Authorized;
+	}
+
+	private string? GetUserIdFromToken(string token) {
+		var securityToken = new JwtSecurityTokenHandler().ReadToken(token);
+		if (securityToken is not JwtSecurityToken jwtSecurityToken)
+		{
+			return null;
+		}
+		return jwtSecurityToken.Claims.FirstOrDefault(claim => claim.Type.Equals(JwtRegisteredClaimNames.Sub))?.Value;
+	}
+}
